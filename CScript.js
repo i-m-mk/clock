e41416a6ea23ed2d48b178899eeeb28efc622b92
i-m-mk <mayank4334@gmail.com>
2021-08-13 00:39:27 +0530

    Optimising

--- CScript.js
+++ CScript.js
@@ -1,9 +1,12 @@
-var time;
-var noon;
+var dateInfo = new Date();
+// determining current day, month and year.
+var day = dateInfo.getDate();
+var month = dateInfo.getMonth()+1;
+var year = dateInfo.getFullYear();
+var leapYear = (year%4===0) ? true : false;
 
 function getTime(){
 	var dateInfo = new Date();
-
 	var hours = dateInfo.getHours();
 	var minutes = dateInfo.getMinutes();
 	var seconds = dateInfo.getSeconds();
@@ -27,12 +30,6 @@ function getTime(){
 	}
 
 function getDate(changeDate = 0){
-	var dateInfo = new Date();
-	// determining current day, month and year.
-	var day = dateInfo.getDate();
-	var month = dateInfo.getMonth()+1;
-	var year = dateInfo.getFullYear();
-	var leapYear = (year%4===0) ? true : false;
 
 	// adding 0 if date and month are less than 10.
 	if (changeDate==1){
@@ -46,9 +43,6 @@ function getDate(changeDate = 0){
 	// putting today's date as string to display.
 	todayDate = document.getElementById("todayDate");
 	todayDate.innerText = `${day}:${month}:${year}`;
-
-	halleyCometCoutdown(day, month, year, leapYear);
-	marioBirthdayCountdown(day, month, year, leapYear);
 }
 
 function sessionOfDay(hours, timeOfDay="AM"){
@@ -69,96 +63,70 @@ function sessionOfDay(hours, timeOfDay="AM"){
 	dayCategory.innerText = `What a lovely ${session} it is!`;
 }
 
-function halleyCometCoutdown(day, month, year, leapYear){
-	let cometTimeDay = 28;
-	let cometTimeMonth = 7;
-	let cometTimeYear = 2061;
+function yearlyEventCountdown(eventDay, eventMonth, eventYear, eventID, eventMessage){
+	let dayOfEvent = eventDay;
+	let monthOfEvent = eventMonth;
+	let yearOfEvent = eventYear;
+	let eventName = eventID;
+	let previousMonth = monthOfEvent-1;
+	let addDays = 0;
 
-	if(cometTimeMonth<month && cometTimeDay>=day){
-		yearDifference = cometTimeYear-year-1;
-		monthDifference = 12+(cometTimeMonth-month);
-		dayDifference = cometTimeDay-day;
-		mario = document.getElementById("halleyTimeText");
-		mario.innerText = `${yearDifference} years ${monthDifference} months ${dayDifference} days left`;
+	addDays = (previousMonth!=2) ? ((previousMonth==1 || previousMonth==3 || previousMonth==5 || previousMonth==7
+				|| previousMonth==8 || previousMonth==10 || previousMonth==12) ? 31 : 30) : 28;
+	
+	console.log(addDays);
+	if(monthOfEvent<month && dayOfEvent>=day){
+		yearDifference = yearOfEvent-year-1;
+		monthDifference = 12+(monthOfEvent-month);
+		dayDifference = dayOfEvent-day;
 	}
-	else if(cometTimeMonth<month && cometTimeDay<day){
-		yearDifference = cometTimeYear-year-1;
-		monthDifference = 12+(cometTimeMonth-month);
+	else if(monthOfEvent<month && dayOfEvent<day){
+		yearDifference = yearOfEvent-year-1;
+		monthDifference = 11+(monthOfEvent-month);
 		if(leapYear)
-			dayDifference = 31+(cometTimeDay-day);
+			dayDifference = (addDays+1)+(dayOfEvent-day);
 		else
-			dayDifference = 30+(cometTimeDay-day);
-		mario = document.getElementById("halleyTimeText");
-		mario.innerText = `${yearDifference} years ${monthDifference} months ${dayDifference} days left`;
+			dayDifference = addDays+(dayOfEvent-day);
 	}
-	else if(cometTimeMonth>=month && cometTimeDay<day){
-		if (cometTimeMonth-month==0){
-			cometTimeYear = cometTimeYear+76;
-			monthDifference = 0;
-		}
-		else
-			monthDifference = cometTimeMonth-month;
+	else if(monthOfEvent>=month && dayOfEvent<day){
+		yearDifference = yearOfEvent+76;
+		monthDifference = (monthOfEvent-month==0) ? 11 : (monthOfEvent-month);
 		if(leapYear)
-			dayDifference = 31+(cometTimeDay-day);
+			dayDifference = (addDays+1)+(dayOfEvent-day);
 		else
-			dayDifference = 30+(cometTimeDay-day);
-		mario = document.getElementById("halleyTimeText");
-		mario.innerText = `${yearDifference} years ${monthDifference} months ${dayDifference} days left`;
+			dayDifference = addDays+(dayOfEvent-day);
 	}
-	else if(cometTimeMonth==month && cometTimeDay==day){
-		mario = document.getElementById("halleyTimeText");
-		mario.innerText = `Don't miss the Comet tonight!!`;
+	else if(monthOfEvent==month && dayOfEvent==day && yearOfEvent==year){
+		selectElement = document.getElementById(eventName);
+		selectElement.innerText = `${eventMessage}`;
+		eventName = " ";
 	}
 	else{
-		monthDifference = cometTimeMonth-month;
-		dayDifference = cometTimeDay-day;
-		mario = document.getElementById("halleyTimeText");
-		mario.innerText = `${yearDifference} years ${monthDifference} months ${dayDifference} days left`;
+		yearDifference = yearOfEvent-year;
+		monthDifference = monthOfEvent-month;
+		dayDifference = dayOfEvent-day;
+	}
+
+	console.log(eventName);
+	if (eventName!= " " && eventYear==0){
+		selectElement = document.getElementById(eventName);
+		selectElement.innerText = `0 years ${monthDifference} months ${dayDifference} days left`;
+	}
+	else{
+		selectElement = document.getElementById(eventName);
+		selectElement.innerText = `${yearDifference} years ${monthDifference} months ${dayDifference} days left`;
 	}
 }
 
-function marioBirthdayCountdown(day, month, year, leapYear){
-	let marioBirthDay = 10;
-	let marioBirthMonth = 3;
-	let yearDifference = 0;
-
-
-	if(marioBirthMonth<month && marioBirthDay>=day){
-		monthDifference = 12+(marioBirthMonth-month);
-		dayDifference = marioBirthDay-day;
-		mario = document.getElementById("marioTimeText");
-		mario.innerText = `${yearDifference} years ${monthDifference} months ${dayDifference} days left`;
-	}
-	else if(marioBirthMonth<month && marioBirthDay<day){
-		monthDifference = 11+(marioBirthMonth-month);
-		if(leapYear)
-			dayDifference = 29+(marioBirthDay-day);
-		else
-			dayDifference = 28+(marioBirthDay-day);
-		mario = document.getElementById("marioTimeText");
-		mario.innerText = `${yearDifference} years ${monthDifference} months ${dayDifference} days left`;
-	}
-	else if(marioBirthMonth>=month && marioBirthDay<day){
-		monthDifference = (marioBirthMonth-month==0) ? 11 : (marioBirthMonth-month);
-		if(leapYear)
-			dayDifference = 29+(marioBirthDay-day);
-		else
-			dayDifference = 28+(marioBirthDay-day);
-		mario = document.getElementById("marioTimeText");
-		mario.innerText = `${yearDifference} years ${monthDifference} months ${dayDifference} days left`;
-	}
-	else if(marioBirthMonth==month && marioBirthDay==day){
-		mario = document.getElementById("marioTimeText");
-		mario.innerText = `HAPPY BIRTHDAY MARIO!!`;
-	}
-	else{
-		monthDifference = marioBirthMonth-month;
-		dayDifference = marioBirthDay-day;
-		mario = document.getElementById("marioTimeText");
-		mario.innerText = `${yearDifference} years ${monthDifference} months ${dayDifference} days left`;
-	}
-}
-
+//essential function calls
 getDate();
 getTime();
 setInterval(getTime, 1000);
+
+//Big Events Function Calls
+/*Halley's Comet*/ yearlyEventCountdown(21, 7, 2061, "eventTimeText1", "Don't miss the sky, Tonight!");
+
+//Yearly Function Calls
+/*Mario's Birthday*/ yearlyEventCountdown(10, 3, 0, "eventTimeText2", "Happy Birthday Mario!!");
+/*Longest Day*/ yearlyEventCountdown(21, 6, 0, "eventTimeText3", "What! Why is it not Night yet?");
+/*Longest Night*/ yearlyEventCountdown(21, 12, 0, "eventTimeText4", "The day of Darkness is here");
